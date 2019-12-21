171baaf27359b71df71204e05bbd12edeab96ba2
Oksana Matskiv <47506194+o-matskiv@users.noreply.github.com>
2019-12-21 19:51:16 +0200

    Add files via upload

---
+++ src/test/java/ua/edu/ucu/AsIntStreamTest.java
@@ -0,0 +1,65 @@
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AsIntStreamTest extends TestCase {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void of() {
+    }
+
+    @Test
+    public void average() {
+    }
+
+    @Test
+    public void max() {
+    }
+
+    @Test
+    public void min() {
+    }
+
+    @Test
+    public void count() {
+    }
+
+    @Test
+    public void sum() {
+    }
+
+    @Test
+    public void filter() {
+    }
+
+    @Test
+    public void forEach() {
+    }
+
+    @Test
+    public void map() {
+    }
+
+    @Test
+    public void flatMap() {
+    }
+
+    @Test
+    public void reduce() {
+    }
+
+    @Test
+    public void toArray() {
+    }
+}
